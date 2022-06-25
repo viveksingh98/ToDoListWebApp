0c51dde84863d911072350a2e4dd8691e5fb23f8
viveksingh98 <viveksingh98@gmail.com>
2022-06-25 11:21:55 +0530

    Add files via upload
    Adding product model

---
+++ ToDoListWebAppPages/ToDoMVCPage.cs
@@ -0,0 +1,16 @@
+﻿/* ***************************************************************
+* Test Automation Framework - Product Model
+* Author: Vivek Singh
+* Date/Time: 06/24/2022
+*****************************************************************/
+
+using System;
+
+namespace ToDoListWebAppPages
+{
+    public class ToDoMVCPage
+    {
+
+
+    }
+}
